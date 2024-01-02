b56ced97a0791c220ecdcdbd2d0f8d474316a046
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-01-02 23:37:07 +0530

    [ServletMVC-002] - Food POJO Class and JDBC Connection class

--- src/main/java/com/vibhorkolte/foodmenu/FoodCart.java
+++ src/main/java/com/vibhorkolte/foodmenu/FoodCart.java
@@ -7,14 +7,15 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 
 public class FoodCart extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// get the data from database(model)
-		String[] foodItems = {"momo","biryani","pizza"};
-		request.setAttribute("foodItems",foodItems);
+		List<Food> foodList = FoodCartDbUtil.getFoodList();
+		request.setAttribute("foodItems", foodList);
 		
 		// redirect to different page (view)
 		RequestDispatcher dispatcher = request.getRequestDispatcher("show-food.jsp");
